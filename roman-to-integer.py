c907d55ae13f3079360be89032e1cb00abe12a76
Maciej Zelder <maciej.zelder@wp.pl>
2024-07-30 09:26:43 +0200

    30.07

--- roman-to-integer.py
+++ roman-to-integer.py
@@ -1,37 +1,64 @@
-
-def romanToInt(s):
-    numbers = [1, 5, 10, 50, 100, 500, 1000]
-    roman = {
-        "I": 1,
-        "V": 2,
-        "X": 3,
-        "L": 4,
-        "C": 5,
-        "D": 6,
-        "M": 7 
-    }
-    result = 0
-
-    i = 0
-    while i < len(s) - 1:
-        current_char = s[i]
-        next_char = s[i+1]
-
-        # if i == len(s) - 2:
-        #     print("1")
-        #     result += numbers[roman[next_char] - 1]
-        print(f"CURRENT_CHAR: {current_char}")
-        print(f"NEXT_CHAR: {next_char}")
-        if roman[next_char] % 2 == 1 and roman[next_char] - roman[current_char] == (1 or 2):
-            print("1")
-            result += numbers[roman[next_char] - 1] - numbers[roman[current_char] - 1]
-        else:
-            print(2)
-            result += numbers[roman[current_char] - 1]
-
-        i += 1
+# My solution
+   class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000 
+        }
+        n = len(s)
+        result = 0
+        i = 0
     
-    print(result)
-    return result
-    
-romanToInt("IV")
+        if n <= 1:
+            return roman[s[0]]
+        while i < n - 1:
+            current_char = s[i]
+            next_char = s[i+1]
+            if current_char == "I" and next_char in ("V", "X"):
+                result += roman[next_char] - roman[current_char]
+                i += 1
+            elif current_char == "X" and next_char in ("L", "C"):
+                result += roman[next_char] - roman[current_char]
+                i += 1
+            elif current_char == "C" and next_char in ("D", "M"):
+                result += roman[next_char] - roman[current_char]
+                i += 1
+            else:
+                if i == n - 2: 
+                    result += roman[next_char]
+                result += roman[current_char]
+                
+            i += 1
+        
+        return result
+
+# GPT solution
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000 
+        }
+        
+        result = 0
+        n = len(s)
+        
+        for i in range(n):
+            # If we're at the last character or the current character is not less than the next one
+            if i == n - 1 or roman[s[i]] >= roman[s[i + 1]]:
+                result += roman[s[i]]
+            else:
+                result -= roman[s[i]]
+        
+        return result
